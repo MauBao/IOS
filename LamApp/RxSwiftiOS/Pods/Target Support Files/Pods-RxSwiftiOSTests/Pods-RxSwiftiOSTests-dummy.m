593074a037c1cc3a09dd517f5c447ecb854e9574
Bao Ngo Mau <ngomaubaodhcn1b@gmail.com>
2018-10-11 15:07:49 +0700

    RxSwift done search SearchBar

---
+++ LamApp/RxSwiftiOS/Pods/Target Support Files/Pods-RxSwiftiOSTests/Pods-RxSwiftiOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftiOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftiOSTests
+@end
---
+++ LamApp/RxSwiftiOS/RxSwiftiOS/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  RxSwiftiOS
+//
+//  Created by Bao Ngo Mau on 10/11/18.
+//  Copyright © 2018 Bao Ngo Mau. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var shownCities = [String]() // Data source for UITableView
+    let allCities = ["Oklahoma", "Chicago", "Moscow", "Danang", "Vancouver", "Praga"] // Mocked API data source
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //  theo dõi thuộc tính rx.text của UISearchBar
+         searchBar.rx.text // Observable property
+         .orEmpty // Make it non-optional
+         .subscribe(onNext: { [unowned self] query in // Here we will be notified of every new value
+         self.shownCities = self.allCities.filter { $0.hasPrefix(query) } // We now do our "API Request" to find cities
+         self.tableView.reloadData() // And reload table view data
+         })
+            .disposed(by: disposeBag)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return (shownCities.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default , reuseIdentifier: "CityCell")
+        cell.textLabel?.text = shownCities[indexPath.row]
+        
+        return cell
+    }
+    
+}
+
