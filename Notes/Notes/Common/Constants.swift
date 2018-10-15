72cc243224555ffc0f0ac45ddc78aac1fb75d0ed
Bao Ngo Mau <ngomaubaodhcn1b@gmail.com>
2018-10-15 18:18:17 +0700

    update constants

--- Notes/Notes/Common/Constants.swift
+++ Notes/Notes/Common/Constants.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class Constants {
 
+    static let nill = ""
+    
     // ShowAlert
     static let titleShowAlertMessage = "Message"
     static let buttonShowAlertOK = "OK"
+    
+    // Title Alert
 
+    // Name Code data
+    static let lblNote = "Note: "
+    static let lblNameNote = "Name: "
+    static let lblMoneyNote = "Money: "
+    
+    // Name items UITableView
+    static let itemShare = "Share"
+    static let itemDelete = "Delete"
+
+    // Name file xib
+    static let CustomNoteTableViewCell = "CustomNoteTableViewCell"
+    
+    
+    // Name plancehold
+    static let planceSearch = "Search"
+    static let planceitemEntryTextView = "Type anything..."
 }
